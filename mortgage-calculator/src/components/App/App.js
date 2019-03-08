90acab72017bb34d310da2390694a6e44823ea3b
Sergio Miranda <sjamr10@gmail.com>
2019-03-08 10:22:26 -0300

    Move Container into its own file

--- mortgage-calculator/src/components/App/App.js
+++ mortgage-calculator/src/components/App/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
+import Container from '../Container';
 import Header from '../Header.js';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: auto;
-  padding: 20px;
-  width: 70%;
-`;
 
 export default class extends Component {
   render() {
